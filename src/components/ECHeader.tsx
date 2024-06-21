22c49984cb910730736a7cd3f2ee35726e74addc
umerprogrammer <umer.programmar@example.com>
2024-06-21 14:59:54 +0500

    21Jun2024 Header

--- src/components/ECHeader.tsx
+++ src/components/ECHeader.tsx
@@ -3,23 +3,37 @@ import Badge, { BadgeProps } from '@mui/material/Badge';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { NavCartCount } from '../App';
 import { Link } from 'react-router-dom';
-
+import SearchIcon from '@mui/icons-material/Search';
 
 
 export default function ECHeader() {
     const cartCount = useContext(NavCartCount);
-    const [totalCount,setTotalCount ] = useState(cartCount);
-    
+    const [totalCount, setTotalCount] = useState(cartCount);
+
 
     return <>
         <nav className="navbar navbar-expand-sm navbar-dark bg-transparent text-dark px-2  ">
-            <a className="navbar-brand text-dark" href="#">Shops</a>
+            <a className="navbar-brand text-dark w-25" href="#">Shops</a>
+            <div className='border main-menu-search'>
+                <div className="row m-0">
+                    <div className="col-sm-4 p-0 pe-1">
+                        <input type="text" placeholder='Categories' className='border-0 p-1 border-end w-100' />
+                    </div>
+                    <div className="col-sm-7 p-0 ">
+                            <input type="text" placeholder='item....' className='border-0 p-1 border-end  w-100' />
+                                
+                    </div>
+                    <div className="col-md-1 p-0 d-flex align-items-center justify-content-center">
+                    <SearchIcon />
+                    </div>
+                </div>
+            </div>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto ">
-                <li className="nav-item ">
+                    <li className="nav-item ">
                         <a className="nav-link text-dark" href="/products">Products</a>
                     </li>
                     <li className="nav-item">
